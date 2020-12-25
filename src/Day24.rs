225031087590f9f52502e65bb48131a98d7c8899
Rolf Staflin <github@piro.se>
2020-12-25 01:33:07 +0100

    Day 24 done.

--- src/Day24.rs
+++ src/Day24.rs
@@ -4,7 +4,6 @@ use std::collections::HashSet;
 pub fn day24() {
     let contents = fs::read_to_string("Input/Day24.txt").expect("Couldn't read the file");
 
-    let mut part1 = 0;
     let mut flipped_tiles = HashSet::new();
     for line in contents.lines() {
         let mut iterator = line.chars().into_iter();
@@ -43,8 +42,56 @@ pub fn day24() {
             flipped_tiles.insert(position.to_owned());
         }
     }
-
     println!("Part 1: {}", flipped_tiles.len());
-    // let mut part2 = 0;
-    // println!("Part 2: {}", part2);
+
+    for _ in 0..100 {
+        flipped_tiles = flip_floor(&flipped_tiles);
+    }
+    println!("Part 2: {}", flipped_tiles.len());
+}
+
+fn flip_floor(before: &HashSet<(i32, i32)>) -> HashSet<(i32, i32)> {
+    let mut after = HashSet::new();
+    let mut min = (0, 0);
+    let mut max = (0, 0);
+    for tile in before {
+        if min.0 > tile.0 { min.0 = tile.0 }
+        if min.1 > tile.1 { min.1 = tile.1 }
+        if max.0 < tile.0 { max.0 = tile.0 }
+        if max.1 < tile.1 { max.1 = tile.1 }
+    }
+
+    for x in min.0 - 1..=max.0 + 1 {
+        for y in min.1 - 1..=max.1 + 1 {
+            let tile = (x, y);
+            if before.contains(&tile) {
+                match count_neighbors(tile, before) {
+                    1 | 2 => { after.insert(tile); },
+                    _ => {}
+                }
+            } else {
+                if count_neighbors(tile, before) == 2 {
+                    after.insert(tile);
+                }
+            }
+        }
+    }
+
+    after
+}
+
+fn count_neighbors(tile: (i32, i32), floor: &HashSet<(i32, i32)>) -> i32 {
+    let mut count = 0;
+    if floor.contains(&(tile.0 - 1, tile.1)) { count += 1 }
+    if floor.contains(&(tile.0 + 1, tile.1)) { count += 1 }
+    if floor.contains(&(tile.0, tile.1 - 1)) { count += 1 }
+    if floor.contains(&(tile.0, tile.1 + 1)) { count += 1 }
+    if tile.1 % 2 == 0 {
+        if floor.contains(&(tile.0 - 1, tile.1 - 1)) { count += 1 }
+        if floor.contains(&(tile.0 - 1, tile.1 + 1)) { count += 1 }
+    } else {
+        if floor.contains(&(tile.0 + 1, tile.1 - 1)) { count += 1 }
+        if floor.contains(&(tile.0 + 1, tile.1 + 1)) { count += 1 }
+    }
+    count
 }
