5297aa05b2b9283992cb58761c4ceb1e86f93c5f
Rolf Staflin <github@piro.se>
2021-01-03 00:30:05 +0100

    Day 23 done.

--- src/Day23.rs
+++ src/Day23.rs
@@ -1,7 +1,8 @@
 use std::time::Instant;
-use std::cmp::min;
+use num_format::{ToFormattedString, Locale};
 
-const ONE_MILLION: usize = 1_000_000;
+const ONE_MILLION: usize =  1_000_000;
+const TEN_MILLION: usize = 10_000_000;
 
 pub fn day23() {
     let input = "394618527";
@@ -40,8 +41,6 @@ pub fn day23() {
     }
     println!("Part 1: {}", part1);
     println!("Part 2: {}", part2(input));
-    println!("Control: {}", part2("389125467"));
-    println!("(Should be 934001 * 159792 = 149245887792)");
 }
 
 
@@ -58,37 +57,32 @@ fn part2(input: &str) -> i128 {
         i += 1;
     }
 
-    let mut current_index = 2;
+    let mut current_index = 0;
     let mut current_label = cups[current_index];
-    let mut now = Instant::now();
+    let now = Instant::now();
 
-    for turn in 1..=ONE_MILLION * 5 {
-        if turn % 100000 == 0 {
-            println!("turn:   {:?}", turn);
-            println!("Execution time: {:?}", Instant::now().checked_duration_since(now).unwrap());
-            now = Instant::now();
+    for turn in 1..=TEN_MILLION / 2 {
+        if turn % 5_000 == 0 {
+            println!("Calculating... Performimg turn {} of {} after {:?}", (turn * 2).to_formatted_string(&Locale::en),
+                     TEN_MILLION.to_formatted_string(&Locale::en), Instant::now().checked_duration_since(now).unwrap());
         }
         make_move(&cups, &mut cups2, current_index);
-//        println!("result:   {:?}", &cups2[0..20]);
-
         current_index = (index_of2(current_label, &cups2) + 1).rem_euclid(ONE_MILLION);
         current_label = cups2[current_index];
+
         make_move(&cups2, &mut cups, current_index);
-//        println!("result 2: {:?}", &cups[0..20]);
         current_index = (index_of2(current_label, &cups) + 1).rem_euclid(ONE_MILLION);
         current_label = cups[current_index];
     }
     let cup_1_index = index_of2(1, &cups);
-    println!("result:   {:?}", &cups);
     let cup1 = cups[(cup_1_index + 1).rem_euclid(ONE_MILLION)] as i128;
     let cup2 = cups[(cup_1_index + 2).rem_euclid(ONE_MILLION)] as i128;
-    println!("Part 2: {} * {} == {}", cup1, cup2, cup1 * cup2);
+
     cup1 * cup2
 }
 
 fn make_move(source: &[i32; ONE_MILLION], result: &mut [i32; ONE_MILLION], current_index: usize) {
     let current_label: i32 = source[current_index];
-//    println!("Current cup {}", current_label);
 
     let mut picked_up:[i32; 3] = [0; 3];
     let mut pickup_index = current_index + 1;
@@ -103,19 +97,13 @@ fn make_move(source: &[i32; ONE_MILLION], result: &mut [i32; ONE_MILLION], curre
         destination_label = (destination_label - 2).rem_euclid(ONE_MILLION as i32) + 1;
     }
     let destination_index = index_of2(destination_label, &source);
-    // println!("source:      {:?} at index {}", current_label, current_index);
-    // println!("destination: {:?} at index {}", destination_label, destination_index);
 
     if current_index < destination_index {
-        // ....1xxx,,,,2____
-        // ....1,,,,2xxx____
         result[..=current_index].copy_from_slice(&source[..=current_index]);
         result[current_index + 1..=destination_index - 3].copy_from_slice(&source[current_index + 4..=destination_index]);
         result[destination_index - 2..=destination_index].copy_from_slice(&source[current_index + 1..current_index + 4]);
         result[destination_index + 1..].copy_from_slice(&source[destination_index + 1..]);
     } else {
-        // ....2,,,,1xxx____
-        // ....2xxx,,,,1____
         if current_index >= ONE_MILLION - 4 {
             let overlap = (current_index + 4).rem_euclid(ONE_MILLION);
             result[..=destination_index - overlap].copy_from_slice(&source[overlap..=destination_index]);
@@ -137,8 +125,6 @@ fn make_move(source: &[i32; ONE_MILLION], result: &mut [i32; ONE_MILLION], curre
 }
 
 
-
-
 fn index_of(label: i32, cups: &Vec<i32>) -> usize {
     for i in 0..cups.len() {
         if cups[i] == label {
@@ -156,13 +142,3 @@ fn index_of2(label: i32, cup_array: &[i32; ONE_MILLION]) -> usize {
     }
     panic!("Couldn't find {} in {:?}", label, &cup_array[0..20])
 }
-
-// -- move 1000 --
-// Execution time: 90.936737205s
-
-// Execution time: 1.709578022s
-// Part 2: 382911 * 455139 == -1815929507
-// 1815929507 is too low
-// 174277729629 is too low
-// 470976377428 is too low
-//(Should be 934001 * 159792 = 149245887792)
